1cb11eb2cb1599f0b157a42e89d576ac2e8ead31
diasgc <gustavocdias@me.com>
2021-10-07 00:19:10 +0100

    brunsli w64+

--- brunsli.sh
+++ brunsli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #     Aa8 Aa7 A86 A64
-# NDK ++  ++  ++  ++  clang
-# GNU ++   .   .   .  gcc
-# WIN  .   .   .   .  clang/gcc
+# NDK +++ +++ +++ +++ clang
+# GNU ...  .   .   .  gcc
+# WIN +++  .   .  +++ clang/gcc
 
 lib='brunsli'
 pkg='brunslienc-c'
@@ -22,38 +22,55 @@ lst_inc='brunsli/jpeg_data_writer.h
 	brunsli/jpeg_data_reader.h
 	brunsli/encode.h'
 lst_lib='libbrunslidec-c libbrunslienc-c'
-lst_bin=''
+lst_bin='dbrunsli cbrunsli'
 
 req_pcforlibs=$lst_lib
+
+CFG="-DBUILD_TESTING=OFF -DINSTALL_GTEST=OFF"
 
 . xbuilder.sh
 
 source_patch(){
     pushdir $SRCDIR
-    
-    # clone submodules
     git submodule update --init
-    
-    # patch to build both static and shared
-    sed -i 's/target_link_libraries(brunslidec-c PRIVATE brunslidec-static)/set_target_properties(brunslidec-c brunslidec-static PROPERTIES OUTPUT_NAME brunslidec-c)\ntarget_link_libraries(brunslidec-c PRIVATE brunslidec-static)/' brunsli.cmake
-    sed -i 's/target_link_libraries(brunslienc-c PRIVATE brunslienc-static)/set_target_properties(brunslienc-c brunslienc-static PROPERTIES OUTPUT_NAME brunslienc-c)\ntarget_link_libraries(brunslienc-c PRIVATE brunslienc-static)/' brunsli.cmake
-    sed -i 's/TARGETS brunslidec-c brunslienc-c/TARGETS brunslidec-c brunslienc-c brunslidec-c brunslienc-static brunslidec-static/' brunsli.cmake
-
     popdir
-}
-
-build_pkgconfig_file(){
-    create_pkgconfig_file 'brunslidec-c' '-lbrunslidec-c'
-    create_pkgconfig_file 'brunslienc-c' '-lbrunslienc-c'
 }
 
 start
 
-# TODO -------------------------------------
-#  - missing 1 pc file (*dec) in package
-#  - no bin (?)
-#  - strip
+<<'XB_PATCH_CMAKELISTS'
+--- brunsli.cmake	2021-10-06 22:13:05.628152000 +0100
++++ brunsli.cmake	2021-10-06 22:13:09.258152000 +0100
+@@ -74,7 +74,8 @@
+ )
+ 
+ set(BRUNSLI_LIBRARIES brunslicommon-static brunslidec-static brunslienc-static)
+-
++set_target_properties(brunslienc-static PROPERTIES OUTPUT_NAME brunslienc-c)
++set_target_properties(brunslidec-static PROPERTIES OUTPUT_NAME brunslidec-c)
+ if(NOT BRUNSLI_EMSCRIPTEN)
+ add_library(brunslidec-c SHARED
+   c/dec/decode.cc
+@@ -153,7 +154,7 @@
+ # Installation
+ if(NOT BRUNSLI_EMSCRIPTEN)
+   install(
+-    TARGETS brunslidec-c brunslienc-c
++    TARGETS brunslidec-c brunslienc-c brunslidec-static brunslienc-static
+     ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+     LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+   )
+@@ -171,6 +172,8 @@
+   RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/artifacts"
+ )
+ 
++install(TARGETS cbrunsli dbrunsli RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
++
+ if (${BUILD_TESTING})
+ 
+ include(GoogleTest)
 
+XB_PATCH_CMAKELISTS
 
 # Filelist
 # --------
@@ -68,6 +85,9 @@ start
 # include/brunsli/encode.h
 # lib/libbrunslidec-c.so
 # lib/pkgconfig/brunslienc-c.pc
+# lib/pkgconfig/brunslidec-c.pc
 # lib/libbrunslienc-c.so
 # lib/libbrunslidec-c.a
 # lib/libbrunslienc-c.a
+# bin/dbrunsli
+# bin/cbrunsli
