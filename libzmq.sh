23130de1360cd246cd819ad0f4202e8b6c49c175
diasgc <gustavocdias@me.com>
2022-01-26 00:46:19 +0000

    upd

--- libzmq.sh
+++ libzmq.sh
@@ -4,11 +4,12 @@
 # GNU  .   .   .   .  gcc
 # WIN  F   .   .   F  clang/gcc
 
+ver='4.3.5'
 lib='libzmq'
-dsc=''
+dsc='ZeroMQ core engine in C++, implements ZMTP/3.1'
 lic='GLP-2.0'
 src='https://github.com/zeromq/libzmq.git'
-cfg='ag'
+cfg='ar'
 eta='0'
 dep='libsodium'
 
@@ -19,6 +20,8 @@ dep='libsodium'
 lst_inc=''
 lst_lib=''
 lst_bin=''
+lst_lic='COPYING COPYING.LESSER AUTHORS'
+lst_pc=''
 
 . xbuilder.sh
 
