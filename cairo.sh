b87e5d7be0ae96a2d62218a6ed8f35d84893dd32
diasgc <15062495+diasgc@users.noreply.github.com>
2022-01-31 14:33:34 +0000

    upd

--- cairo.sh
+++ cairo.sh
@@ -4,12 +4,24 @@ lib='cairo'
 apt='libcairo2-dev'
 dsc='Development files for the Cairo 2D graphics library'
 lic='LGPL-2.1'
-#src='https://github.com/freedesktop/cairo.git'
-src='https://cairographics.org/releases/cairo-5c-1.20.tar.gz'
-cfg='ar'
-dep='fontconfig'
+src='https://github.com/freedesktop/cairo.git'
+#src='https://cairographics.org/releases/cairo-5c-1.20.tar.gz'
+cfg='meson'
+dep='fontconfig freetype'
 eta='275'
 #CFG="--disable-gtk-doc-html --disable-gl --enable-gobject --enable-pdf --enable-svg --enable-ps"
+cfg_meson='-Dxcb=disabled -Dxlib=disabled -Dxlib-xcb=disabled -Dtests=disabled -Dquartz=disabled -Dsymbol-lookup=disabled -Dspectre=disabled'
+
+dev_bra='master'
+dev_vrs=''
+stb_bra='1.16'
+stb_vrs=''
+
+lst_inc=''
+lst_lib=''
+lst_bin=''
+lst_lic='LICENSE AUTHORS'
+lst_pc=''
 
 . xbuilder.sh
 
